3bd15b95e0837d03e65591b5291eb3080c03efa5
Bogdan Vilimonovic <bogdan.vilimonovic.ibdev@gmail.com>
2023-11-09 09:11:28 +0100

    added listing

---
+++ src/items/items.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+import { UpdateItemDto } from './dto/update-item.dto';
+import { EntityManager, Repository } from 'typeorm';
+import { Item } from './entities/item.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Listing } from './entities/listing.entity';
+
+@Injectable()
+export class ItemsService {
+  constructor(
+    @InjectRepository(Item)
+    private readonly itemsRepository: Repository<Item>,
+    private readonly entityManager: EntityManager 
+  ) {}
+
+  async create(createItemDto: CreateItemDto) {
+    const listing = new Listing({
+      ...createItemDto.listing,
+      rating: 0
+    });
+    const item = new Item ({
+      ...createItemDto,
+      listing
+    });
+    await this.itemsRepository.save(item);
+  }
+
+  async findAll() {
+    return this.itemsRepository.find();
+  }
+
+  async findOne(id: number) {
+    return this.itemsRepository.findOneBy({ id });
+  }
+
+  async update(id: number, updateItemDto: UpdateItemDto) {
+    const item = await this.itemsRepository.findOneBy({ id });
+    item.public = updateItemDto.public;
+    await this.entityManager.save(item);
+  }
+
+  async remove(id: number) {
+    await this.itemsRepository.delete({ id });
+  }
+}
